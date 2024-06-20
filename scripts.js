d65688c546fa6926daece8cd0943ab10636ba11b
ibrahim99elaw <ibrahimhanielaw@gmail.com>
2024-06-20 19:36:39 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 // script.js
 function calculateZakat() {
+    const goldPrice = document.getElementById('gold-price').value*85;
     const amount = document.getElementById('amount').value;
-    if (amount && amount > 0) {
-        const zakat = amount * 0.025;
-        document.getElementById('result').innerText = `زكاة المبلغ هي: ${zakat} جنيه `;
+
+    if (goldPrice && amount && amount > 0) {
+        const goldPriceThreshold = parseFloat(goldPrice);
+
+        if (amount >= goldPriceThreshold) {
+            const zakat = amount * 0.025;
+            document.getElementById('result').innerText = `زكاة المبلغ هي: ${zakat} جنيه`;
+        } else {
+            document.getElementById('result').innerText = 'المبلغ المدخل أقل من النصاب الشرعي للزكاة.';
+        }
     } else {
-        document.getElementById('result').innerText = 'يرجى إدخال مبلغ صحيح.';
+        document.getElementById('result').innerText = 'يرجى إدخال مبلغ وسعر الذهب صحيحين.';
     }
 }
